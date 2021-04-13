c72ac9074f840c766e39c09f591ef339e8102259
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-04-12 22:14:49 -0300

    Pastas e DbContext

--- ABAPAI.Infra/Contexts/DataContext.cs
+++ ABAPAI.Infra/Contexts/DataContext.cs
@@ -19,7 +19,20 @@ namespace ABAPAI.Infra.Contexts
         {
             modelBuilder.Entity<Staff>().ToTable("Staff");
             modelBuilder.Entity<Staff>().HasKey(x=> x.Id);
-            
+            modelBuilder.Entity<Staff>().Property(x => x.Name_user).HasColumnType("varchar(70)");
+            modelBuilder.Entity<Staff>().Property(x => x.Name).HasColumnType("varchar(100)");
+            modelBuilder.Entity<Staff>().Property(x => x.Email).HasColumnType("varchar(100)");
+            modelBuilder.Entity<Staff>().Property(x => x.Password).HasColumnType("varchar(100)");
+            modelBuilder.Entity<Staff>().Property(x => x.Role).HasConversion(typeof(string));
+            modelBuilder.Entity<Staff>().Property(x => x.CPF).HasColumnType("varchar()");
+            modelBuilder.Entity<Staff>().Property(x => x.CNPJ).HasColumnType("varchar()");
+            modelBuilder.Entity<Staff>().Property(x => x.StateRegistration).HasColumnType("varchar(9)");
+            modelBuilder.Entity<Staff>().Property(x => x.Description).HasColumnType("varchar(200)");
+            modelBuilder.Entity<Staff>().Property(x => x.DDD).HasColumnType("varchar(3)");
+            modelBuilder.Entity<Staff>().Property(x => x.Phone).HasColumnType("varchar(10)");
+            modelBuilder.Entity<Staff>().Property(x => x.Image).HasColumnType("varchar(120)");
+            // TODO = verificar parâmetro passado pelo blob (azure) para a imagem
+
         }
     }
 }
