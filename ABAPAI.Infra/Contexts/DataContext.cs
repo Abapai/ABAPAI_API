2821a28e6472f92cf64416d89f84999d7558831c
Abner Matheus <abnerm80@gmail.com>
2021-04-21 02:47:10 -0300

    Ajustes

--- ABAPAI.Infra/Contexts/DataContext.cs
+++ ABAPAI.Infra/Contexts/DataContext.cs
@@ -1,9 +1,8 @@
-﻿using ABAPAI.Domain.Entities;
+﻿
+using ABAPAI.Domain.Entities;
 using ABAPAI.Domain.Enums;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ABAPAI.Infra.Contexts
 {
