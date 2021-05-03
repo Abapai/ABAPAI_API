300410e43eaca1ac14c087c1981f3b2d8966f26d
Abner Matheus <abnerm80@gmail.com>
2021-05-03 20:53:51 -0300

    Criando testes

--- ABAPAI.Tests/HandlerTests/Staff/AuthenticationStaff_HandlerTests.cs
+++ ABAPAI.Tests/HandlerTests/Staff/AuthenticationStaff_HandlerTests.cs
@@ -16,13 +16,21 @@ namespace ABAPAI.Tests.HandlerTests
     public class AuthenticationStaff_HandlerTests
     {
         private readonly StaffHandler _staffHandler = new StaffHandler(new FakeStaffRepository());
-        private readonly Staff staff = new Staff("abner_math", "name", "abnerm80@gmail.com", "abner123", Roles.STAFF, "09025325864", null, null, false);
+        private readonly Staff staff_valid = new Staff("abner_math", "name", "abnerm80@gmail.com", "abner123", Roles.STAFF, "09025325864", null, null, false);
+        private readonly Staff staff_invalid = new Staff("abner_matheus", "name", "abnerm80@gmail.com", "errado", Roles.STAFF, "09025325864", null, null, false);
 
         [TestMethod]
         public void Dado_um_email_e_senha_de_um_usuario_valido_retornar_token()
         {
-            var result = (GenericCommandResult) _staffHandler.Handle(new AuthenticationStaffCommand(staff.Email, staff.Password));
+            var result = (GenericCommandResult) _staffHandler.Handle(new AuthenticationStaffCommand(staff_valid.Email, staff_valid.Password));
             Assert.AreEqual(true, result.Success);
+        }
+
+        [TestMethod]
+        public void Dado_um_email_e_senha_de_um_usuario_invalido_retornar_false()
+        {
+            var result = (GenericCommandResult)_staffHandler.Handle(new AuthenticationStaffCommand(staff_invalid.Email, staff_invalid.Password));
+            Assert.AreEqual(false, result.Success);
         }
     }
 }
--- ABAPAI.Tests/Repositories/FakeStaffRepository.cs
+++ ABAPAI.Tests/Repositories/FakeStaffRepository.cs
@@ -1,8 +1,10 @@
 ﻿using ABAPAI.Domain.Entities;
 using ABAPAI.Domain.Enums;
 using ABAPAI.Domain.Interfaces.Repositories;
+using ABAPAI.Domain.Utils;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,18 @@ namespace ABAPAI.Tests.Repositories
 {
     public class FakeStaffRepository : IStaffRepository
     {
+
+
+        private List<Staff> _staffs;
+
+        public FakeStaffRepository()
+        {
+            _staffs = new List<Staff>();
+            _staffs.Add(new Staff("abner_math", "name", "abnerm80@gmail.com", "abner123".GetHash(), Roles.STAFF, "09025325864", null, null, false));
+            _staffs.Add(new Staff("abner_matheus", "name2", "abnerm10@gmail.com", "abner123".GetHash(), Roles.STAFF, "09025725861", null, null, false));
+            _staffs.Add(new Staff("jhonatan_med", "name", "jhonatan_med@gmail.com", "jhonatan123".GetHash(), Roles.STAFF, "09022325164", null, null, false));
+            _staffs.Add(new Staff("jhonatan_medeiros", "name", "jhonatan_medeiros@gmail.com", "abner123".GetHash(), Roles.STAFF, "09021325861", null, null, false));
+        }
 
         public void Create(Staff staff)
         {
@@ -23,12 +37,12 @@ namespace ABAPAI.Tests.Repositories
 
         public bool ExistName_user(string name_user)
         {
-            return false;
+            return _staffs.Any(x => x.Name_user == name_user);
         }
 
         public Staff FindStaff(string email, string password)
         {
-            return new Staff("abner_math", "name", "abnerm80@gmail.com", "abner123", Roles.STAFF, "09025325864", null, null, false);
+            return _staffs.Where(x => x.Email == email && x.Password == password).FirstOrDefault();            
         }
     }
 }
