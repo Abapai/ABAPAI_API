443c54bf9b1a01210d12b9e2b2c03eb10da7ec45
Abner Matheus <abnerm80@gmail.com>
2021-05-03 21:04:32 -0300

    Aplicando teste

--- ABAPAI.Tests/HandlerTests/Staff/AuthenticationStaff_HandlerTests.cs
+++ ABAPAI.Tests/HandlerTests/Staff/AuthenticationStaff_HandlerTests.cs
@@ -8,6 +8,8 @@ using ABAPAI.Tests.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Text;
 
 namespace ABAPAI.Tests.HandlerTests
@@ -20,17 +22,20 @@ namespace ABAPAI.Tests.HandlerTests
         private readonly Staff staff_invalid = new Staff("abner_matheus", "name", "abnerm80@gmail.com", "errado", Roles.STAFF, "09025325864", null, null, false);
 
         [TestMethod]
-        public void Dado_um_email_e_senha_de_um_usuario_valido_retornar_token()
+        public void Dado_um_email_e_senha_de_um_staff_valido_retornar_token()
         {
-            var result = (GenericCommandResult) _staffHandler.Handle(new AuthenticationStaffCommand(staff_valid.Email, staff_valid.Password));
-            Assert.AreEqual(true, result.Success);
+            var result = (GenericCommandResult) _staffHandler.Handle(new AuthenticationStaffCommand(staff_valid.Email, staff_valid.Password));            
+            Assert.AreEqual(true, result.Success);            
+            var jwt = new JwtSecurityTokenHandler().ReadJwtToken(result.Data.ToString());
+            Assert.AreEqual(Roles.STAFF.ToString(), jwt.Claims.Where(x => x.Type == "role").Select(x=> x.Value).FirstOrDefault());
         }
 
         [TestMethod]
-        public void Dado_um_email_e_senha_de_um_usuario_invalido_retornar_false()
+        public void Dado_um_email_e_senha_de_um_staff_invalido_retornar_false()
         {
             var result = (GenericCommandResult)_staffHandler.Handle(new AuthenticationStaffCommand(staff_invalid.Email, staff_invalid.Password));
             Assert.AreEqual(false, result.Success);
+
         }
     }
 }
