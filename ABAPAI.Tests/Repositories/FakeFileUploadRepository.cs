26b2af046fa8c2bbc23147e523515e92d53045c3
Abner Matheus <abnerm80@gmail.com>
2021-05-04 21:24:30 -0300

    Atualizando blob na Azure e criação de um fakeRepository

---
+++ ABAPAI.Tests/Repositories/FakeFileUploadRepository.cs
@@ -0,0 +1,25 @@
+﻿using ABAPAI.Domain.Interfaces.Repositories;
+using System;
+using System.Threading.Tasks;
+
+namespace ABAPAI.Tests.Repositories
+{
+    public class FakeFileUploadRepository : IFileUpload
+    {
+        public void DeleteImageAsync(string blobName)
+        {
+
+        }
+
+        public void UpdateImageAsync(string base64Image, string blobName_Actual)
+        {
+
+        }
+
+        public Task<string> UploadBase64ImageAsync(string base64Image)
+        {
+            var filename = Guid.NewGuid().ToString() + ".jpg";
+            return Task.FromResult<string>(filename);
+        }
+    }
+}
