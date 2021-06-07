18f3a2a7f87cf16a08a8723b2cd48a71f084dbdb
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-06-07 20:01:50 -0300

    feat: 'UpdateEventCommand' created

---
+++ ABAPAI.Domain/Commands/Event/UpdateEventCommand.cs
@@ -0,0 +1,18 @@
+﻿using ABAPAI.Domain.Interfaces.Commands;
+using Flunt.Notifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ABAPAI.Domain.Commands.Event
+{
+    public class UpdateEventCommand : Notifiable, ICommand
+    {
+        public UpdateEventCommand() { }
+
+        public void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
